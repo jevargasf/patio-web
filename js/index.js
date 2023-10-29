fd67a9afada3ad92bc7484e932834fdaf2c6f972
jevargasf <116041353+jevargasf@users.noreply.github.com>
2023-10-28 21:14:37 -0300

    scroll título principal versión 1

--- js/index.js
+++ js/index.js
@@ -1,17 +1,9 @@
-/* const section = document.querySelector("section#intro")
-const patio = document.querySelector("h1#zoomscroll")
-const botonToggle = document.getElementById("botonToggle")
-const scrolledClass = "nuevo-estilo"
+const section = document.querySelector("section#intro")
+const patio = document.getElementById("tituloIntro")
 
-
-// toggle class patio con evento click
-botonToggle.addEventListener('click', ()=>{
-    patio.classList.add(scrolledClass)
-})
-    */ 
    
-    //Función que maneja el evento de scroll
-    window.onscroll = function() {
-      console.log("¡Se ha desplazado la página!");
-      document.getElementById("demo").innerHTML = "escroleaste la weá";
-    };
+//Función que maneja el evento de scroll
+window.onscroll = function() {
+    patio.classList = "opacidad"
+    document.getElementById("demo").innerHTML = scrollY;
+};
